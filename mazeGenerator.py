ab3cbd06f3ea34da60bb3abaa1680d127ab54290
Saku Viitalähde <51236897+SakuViitalahde@users.noreply.github.com>
2021-01-09 21:26:05 +0200

    1.0V Maze generator
    1.0 working version, auto scales.

--- mazeGenerator.py
+++ mazeGenerator.py
@@ -1,8 +1,11 @@
 import pygame
+from collections import deque
+import random
+import time
 
 ROWS = 50
 SIZE = 1000
-WIN = pygame.display.set_mode((SIZE+2, SIZE+2))
+WIN = pygame.display.set_mode((SIZE + 2, SIZE + 2))
 pygame.display.set_caption("Maze generator")
 
 RED = (255, 0, 0)
@@ -11,7 +14,8 @@ BLUE = (100, 100, 255)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-class Square():
+
+class Square:
     def __init__(self, row, col, size, total_rows):
         self.row = row
         self.col = col
@@ -19,28 +23,43 @@ class Square():
         self.y = col * size
         self.color = WHITE
         self.size = size
-        self.top = False
-        self.right = False
-        self.bottom = False
-        self.left = False
-    
+        self.top = True
+        self.right = True
+        self.bottom = True
+        self.left = True
+
     def draw(self, win):
         pygame.draw.rect(win, self.color, (self.x, self.y, self.size, self.size))
 
     def draw_walls(self, win):
         # Top = 1, right = 2, bottom = 3 left = 4
         if self.top:
-            pygame.draw.line(win, BLACK, (self.x, self.y), (self.x  + self.size, self.y),2 ) # YLÄVAAKA
+            pygame.draw.line(
+                win, BLACK, (self.x, self.y), (self.x + self.size, self.y), 2
+            )  # YLÄVAAKA
 
         if self.right:
-            pygame.draw.line(win, BLACK, (self.x + self.size, self.y), (self.x + self.size, self.y + self.size), 2) # OIKEA PYSTY
+            pygame.draw.line(
+                win,
+                BLACK,
+                (self.x + self.size, self.y),
+                (self.x + self.size, self.y + self.size),
+                2,
+            )  # OIKEA PYSTY
 
         if self.bottom:
-            pygame.draw.line(win, BLACK, (self.x, self.y + self.size), (self.x + self.size, self.y + self.size), 2) # ALA VAAKA
+            pygame.draw.line(
+                win,
+                BLACK,
+                (self.x, self.y + self.size),
+                (self.x + self.size, self.y + self.size),
+                2,
+            )  # ALA VAAKA
 
         if self.left:
-            pygame.draw.line(win, BLACK, (self.x, self.y), (self.x, self.y + self.size), 2) # VASENPYSTY
-
+            pygame.draw.line(
+                win, BLACK, (self.x, self.y), (self.x, self.y + self.size), 2
+            )  # VASENPYSTY
 
 
 def make_grid(rows, size):
@@ -56,43 +75,100 @@ def make_grid(rows, size):
             square = Square(i, j, gap, rows)
             row.append(square)
         grid.append(row)
-    grid[0][0].color = GREEN # Set Start
-    grid[-1][-1].color = BLUE # Set End
+    grid[0][0].color = GREEN  # Set Start
+    grid[-1][-1].color = BLUE  # Set End
     return grid
 
-def add_walls_for_square(squares, x, y):
-    squares[y][x].top = True
-    squares[y][x].left = True
-    squares[y][x].right = True
-    squares[y][x].bottom = True
-    return squares
+
+def random_next(current, used_spots, squares):
+    #
+    # if squares[0][0] in used_spots:
+    #    print("löyty")
+    possible_moves = []
+
+    # Check possible moves
+    # Left
+    if current.row > 0:
+        if (current.col, current.row - 1) not in used_spots:
+            possible_moves.append(squares[current.row - 1][current.col])
+
+    # Up
+    if current.col > 0:
+        if (current.col - 1, current.row) not in used_spots:
+            possible_moves.append(squares[current.row][current.col - 1])
+
+    # Right
+    if current.row < ROWS - 1:
+        if (current.col, current.row + 1) not in used_spots:
+            possible_moves.append(squares[current.row + 1][current.col])
+
+    # Down
+    if current.col < ROWS - 1:
+        if (current.col + 1, current.row) not in used_spots:
+            possible_moves.append(squares[current.row][current.col + 1])
+
+    if not possible_moves:
+        return None
+
+    # Random next direction
+    next_move = random.choice(possible_moves)
+
+    # Remove walls to moving direction
+    if current.row > 0 and next_move == squares[current.row - 1][current.col]:
+        squares[current.row][current.col].left = False
+        squares[current.row - 1][current.col].right = False
+    elif current.col > 0 and next_move == squares[current.row][current.col - 1]:
+        squares[current.row][current.col].top = False
+        squares[current.row][current.col - 1].bottom = False
+    elif current.row < ROWS - 1 and next_move == squares[current.row + 1][current.col]:
+        squares[current.row][current.col].right = False
+        squares[current.row + 1][current.col].left = False
+    elif current.col < ROWS - 1 and next_move == squares[current.row][current.col + 1]:
+        squares[current.row][current.col].bottom = False
+        squares[current.row][current.col + 1].top = False
+
+    return next_move
+
 
 def calculate_walls(squares):
     """
     Make maze from squares.
     Add walls to maze.
     """
-    grid = {}
-    grid[(0,0)] = squares[0][0]
+    start = squares[0][0]
+    used_spots = {(0, 0)}
+    stack = deque()
+    stack.append(start)
+    current = start
 
-    #Add left wall for window
+    # Add left wall for window
     for x in squares[0]:
         x.left = True
 
-    #Add bottom wall for window
+    # Add bottom wall for window
     for x in squares:
         x[-1].bottom = True
 
-    #Add Right wall for window
+    # Add Right wall for window
     for x in squares[-1]:
         x.right = True
-    
-    #Add left wall for window
+
+    # Add left wall for window
     for x in squares:
         x[0].top = True
-    
-    squares = add_walls_for_square(squares, 1, 1)
+
+    # just for testing
+    while len(stack) > 0:
+        next = random_next(current, used_spots, squares)
+        if next != None:
+            print(len(used_spots))
+            current = next
+            stack.append(current)
+            used_spots.add((current.col, current.row))
+        else:
+            current = stack.pop()
     return squares
+
 
 def draw(window, squares, rows, size):
     window.fill(WHITE)
@@ -104,12 +180,13 @@ def draw(window, squares, rows, size):
         for square in row:
             if square.color != WHITE:
                 square.draw(window)
-    
+
     for row in squares:
         for square in row:
             square.draw_walls(window)
 
     pygame.display.update()
+
 
 def main(win):
     squares = make_grid(ROWS, SIZE)
@@ -121,4 +198,6 @@ def main(win):
                 pygame.quit()
 
         draw(win, squares, ROWS, SIZE)
+
+
 main(WIN)
